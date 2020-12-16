32b1d241ff319d2ab1af1e26a0710185a64698c5
Denis Lira <denislira@live.com>
2020-12-16 18:00:57 -0300

    Iniciando

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>Super Gestão - Sobre Nós</title>
+        <title>Super Gestão - Principal</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="{{ asset('css/estilo_basico.css')}}">
 
--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <title>Super Gestão - Sobre Nós</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="{{ asset('css/estilo_basico.css')}}">
+@extends('site.layouts.basico');
 
-
-    </head>
-
-    <body>
+@section()
         <div class="topo">
 
             <div class="logo">
@@ -53,5 +45,5 @@
                 <img src="{{asset('img/mapa.png')}}">
             </div>
         </div>
-    </body>
-</html>
+        
+@endsection
